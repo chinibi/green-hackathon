3c79583e3e3c9981019327b7796eb3d73a4c51dc
Trevor Pham <trevor.dt.pham@gmail.com>
2016-04-25 13:03:14 -0700

    alert when all 5 boxes checked

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-$(document).ready(function() {
+console.log('javascript loaded')
+$(document).ready( () => console.log('jquery loaded'))
+
+var checks = 0
+
+$(document).ready( () => {
+  $("input[type=checkbox]").click( (event) => {
+    event.target.checked ? checks++ : checks--
+    console.log(checks)
+    if (checks == 5) {
+      alert('badge earned')
+    }
+  })
 })
