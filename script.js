5ef487d1181e12df7d178063588fe01b6cd48494
Trevor Pham <trevor.dt.pham@gmail.com>
2016-04-25 16:05:29 -0700

    mvp

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 console.log('javascript loaded')
-$(document).ready( () => console.log('jquery loaded'))
+$(document).ready( () => {
+  console.log('jquery loaded')
+  $('.badges').hide()
+
+})
 
 var checks = 0
+
+
 
 $(document).ready( () => {
   $("input[type=checkbox]").click( (event) => {
     event.target.checked ? checks++ : checks--
     console.log(checks)
     if (checks == 5) {
-      alert('badge earned')
+      badgeEarned()
     }
   })
 })
+
+$('#see-badges').click( () => {
+  $('#challenge-title').hide()
+  $('#checklist').hide()
+  $('#topic').hide()
+  $('.badges').show()
+})
+
+function badgeEarned() {
+  $('#modal').modal()
+}
